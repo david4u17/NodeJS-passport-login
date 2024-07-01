ea7bd79b379dadb33fd9836dc945e5330d11596e
David Kawadkar <davidkawadkar007@gmail.com>
2024-07-01 12:05:07 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
-// dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
-
-dbPassword='mongodb+srv://david:David%40123@real-time-chat-app.la5rfu2.mongodb.net/chatApp?retryWrites=true&w=majority';
+dbPassword='mongodb+srv://david:<password>@<cluster_name>.la5rfu2.mongodb.net/<db_name>?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
