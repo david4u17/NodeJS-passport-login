3a97ea7568993e63183d1483a6a8159a6cd98dfe
David Kawadkar <davidkawadkar007@gmail.com>
2024-07-02 11:01:59 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 
-// Load User model
+//Load User model
 const User = require('../models/User');
 
 module.exports = function(passport) {
